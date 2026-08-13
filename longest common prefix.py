a9c79ab4e6ecb763ffbf05e1c77246cf262c298b
Poorab Gupta <Poorabkgupta@gmail.com>
2026-08-13 19:54:23 +0530

    working on new

---
+++ longest common prefix.py
@@ -0,0 +1,17 @@
+'''Longest Common Prefix
+You are given a list of strings str
+str. Your task is to find the longest common prefix among all the strings in the list. If there is no common prefix, return -1.
+
+Input Format
+The first line of input will contain a single integer 
+T
+T, denoting the number of test cases.
+Each test case consists of two lines of input:
+The first line contains an integer N, the number of strings.
+The next line contain a string array str.
+Output Format
+For each test case, output the longest common prefix. If there is no common prefix, output -1.'''
+
+str = "flower flow flight"
+result = str.split()
+print(result)
